ec1ef7fe46461b0643df889962b32d8dc3ff995f
Matias maizares <matiasmaizares2017@gmail.com>
2023-04-11 14:44:38 -0300

    pruebas

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -2,13 +2,13 @@
 // @ts-ignore
 const serverSocket = io('http://localhost:8080')
 
-// const div = document.getElementById('container') ?? null
+const div = document.getElementById('container') ?? null
 
-// const template = `
-// <h1>hola mundo2</h1>
-// `
+const template = `
+<h1>hola mundo2</h1>
+`
 
-// const compileProducts = Handlebars.compile(template)
+const compileProducts = Handlebars.compile(template)
 
 // serverSocket.on('reloadProducts', (data) => {
 //   console.log(data)
